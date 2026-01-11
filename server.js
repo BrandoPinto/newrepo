0fd79cec5ddb67b3bd4f3015c2e2b8ce703b2078
unknown <bpintochecya@gmail.com>
2026-01-10 21:07:20 -0500

    one

--- server.js
+++ server.js
@@ -5,26 +5,40 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const env = require("dotenv").config();
+const app = express();
+
+/* ***********************
+ * View Engine and Template
+ ************************/
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout"); // Layout not at views root
+
+/* ***********************
+ * Serve Static Files (CSS, Images, etc.)
+ ************************/
+app.use(express.static('public')); // Servir archivos estáticos desde 'public'
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.get('/', (req, res) => {
+  res.render('index', { title: "Home" }); // Asegúrate de que 'views/index.ejs' existe
+});
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
 const port = process.env.PORT
-const host = process.env.HOST
+const host = process.env.HOST 
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+  console.log(`App listening on ${host}:${port}`);
+});
